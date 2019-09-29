abaa77fec2eccd36d4ffb8eb46ac41283cbf65a9
Rakib-RS <rakib.rscse@gmail.com>
2019-09-29 10:56:13 +0600

    all Variable declared

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,8 +3,28 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvExpression,tvResult;
+    private Button btOne,btTwo,btThree,btFour,btFive,btSix,btSeven,btEight,btNine,btZero,btDot,btAdd,btSub,btDiv,btMul,btEqual,btCe;
+    double v1,v2;
+    ImageButton btDel;
+    private  Button btRoot,btPow,btMod;
+    Boolean opAdd;
+    private static final char ADDITION = '+';
+    private static final char SUBTRACTION = '-';
+    private static final char MULTIPLICATION = '*';
+    private static final char DIVISION = '/';
+    private  static  final char POW ='^';
+    private  static  final char ROOT = 's';
+    private  static  final char MOD = '%';
+    private  static  final char NOT ='N';
+
+    private char CURRENT_ACTION =NOT;
+    String secondValue = new String();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
